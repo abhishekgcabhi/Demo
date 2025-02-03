835e3900b284b3678aedbacc74418cf00a1fb7d5
abhishekgcabhi <abhishek_g@cms.co.in>
2025-02-03 12:18:52 +0530

    first demo project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Practice from './Practice';
+
+function App() {
+  return (
+ <>
+ <Practice></Practice>
+ </>
+  );
+}
+
+export default App;
---
+++ src/Practice.js
@@ -0,0 +1,96 @@
+import React, { useState } from 'react';
+{/*
+function Practice() {
+
+    const [x,y] = useState("hello")
+    console.log(x)
+    function m(){
+     y("world")
+    }
+  return (
+    <div>
+     <button onClick={m}>click</button>
+    </div>
+  );
+}
+
+export default Practice;
+
+togglt of text
+
+
+function Practice() {
+
+    const [x,y] = useState(true)
+    console.log(x)
+    function m(){
+     y(!x)
+    }
+  return (
+    <div>
+        {true && x?"abhi":"raju"}
+     <button onClick={m}>click</button>
+    </div>
+  );
+}
+
+export default Practice;
+
+togglt of img
+function Practice() {
+
+    const [x,y] = useState(true)
+    console.log(x)
+    function m(){
+     y(!x)
+    }
+  return (
+    <div>
+        {x?<img src="logo192.png"/>:<img src="logo512.png"/>}
+     <button onClick={m}>click</button>
+    </div>
+  );
+}
+
+export default Practice;
+
+
+
+//counter
+function Practice() {
+
+    const [x,y] = useState(0)
+    
+    function a(){
+       y(x+1)
+    }
+
+  return (
+    <>
+        {x}
+     <button onClick={a}>click</button>
+    </>
+  );
+}
+
+export default Practice;   */}
+
+function Practice() {
+
+    const [x,y] = useState(0)
+    
+    function a(){
+       y(x+1)
+    }
+
+  return (
+    <>
+        {x}
+     <button onClick={a}>click</button>
+    </>
+  );
+}
+
+export default Practice;
+
+
